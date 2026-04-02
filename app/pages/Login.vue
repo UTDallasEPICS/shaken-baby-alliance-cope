c2fe02f48242e5f2af6df2f04749776c9fbdeacf
PhucT <phutrinh1006@gmail.com>
2026-04-02 15:28:24 -0500

    log in with log out

---
+++ app/pages/Login.vue
@@ -0,0 +1,28 @@
+<script setup lang="js">
+import { ref } from 'vue'
+
+const message = ref('Hello Vue!')
+
+function changeMessage() {
+  message.value = 'You clicked the button!'
+}
+
+// integrate log in and validation logic here
+</script>
+
+<!-- this is for how the log in page will actually look-->
+<template>
+
+  <div class="card">
+   
+  </div>
+</template>
+
+<style scoped>
+
+.card {
+  padding: 20px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+}
+</style>
